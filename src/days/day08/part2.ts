d4ecdc8529e689a06baeae6dc313b9b08d7431ac
dkollar <dkollar5@gmail.com>
2024-12-03 10:10:30 -0800

    Add day templates

---
+++ src/days/day08/part2.ts
@@ -0,0 +1,21 @@
+import { readInput } from "../../utils/input";
+
+/**
+ * Solves the second part of the day 8 challenge
+ * @param useTestInput - Whether to use the test input or the actual input
+ * @returns The number of safe reports
+ * 
+ * test-input.txt result: ???
+ * input.txt result: ???
+ */
+function solve(useTestInput: boolean = false): number {
+  const input = readInput({ day: 8, isTest: useTestInput });
+  return 0;
+}
+
+// If this file is run directly
+if (require.main === module) {
+  console.log("Part 2 Solution:", solve());
+}
+
+export default solve;
---
+++ src/days/day24/part1.ts
@@ -0,0 +1,21 @@
+import { readInput } from "../../utils/input";
+
+/**
+ * Solves the first part of the day 24 challenge
+ * @param useTestInput - Whether to use the test input or the actual input
+ * @returns The number of safe reports
+ * 
+ * test-input.txt result: ???
+ * input.txt result: ???
+ */
+function solve(useTestInput: boolean = false): number {
+  const input = readInput({ day: 24, isTest: useTestInput });
+  return 0;
+}
+
+// If this file is run directly
+if (require.main === module) {
+  console.log("Part 1 Solution:", solve());
+}
+
+export default solve;
